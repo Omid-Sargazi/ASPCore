e2f678bc31d6b5ac5c7d1bd48a4f9bb6573ab945
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-08 08:26:21 +0330

    Bubble Sort

---
+++ Algorithems/Sorting3/Sorts.cs
@@ -0,0 +1,25 @@
+namespace Algorithems.Sorting3
+{
+    public class Sorts
+    {
+        public static void Bubble(int[] arr)
+        {
+            for (int start = arr.Length - 1; start >= 0; start--)
+            {
+                bool swapped = false;
+                for (int j = 0; j < start; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped) break;
+            }
+
+            Console.WriteLine($"Bubble Sort: {string.Join(",",arr)}");
+        }
+    }
+}

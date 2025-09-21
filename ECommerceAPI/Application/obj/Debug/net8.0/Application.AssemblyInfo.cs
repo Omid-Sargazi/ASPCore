5c01e75a0cacbc6ff9679f668b8e5c3052560249
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-21 17:25:51 +0330

    .

--- ECommerceAPI/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ ECommerceAPI/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a450fee8746dfc2fde85f0f47c520bbd3a2ff2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c35e0ade75ea383681387866733f0410198f4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ECommerceAPI/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ ECommerceAPI/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a450fee8746dfc2fde85f0f47c520bbd3a2ff2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c35e0ade75ea383681387866733f0410198f4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
